1f5a9e272332d5f775b15d7a3e5ceca71c53c12f
Martin Olivares <martin.olivarest@utem.cl>
2022-12-24 14:33:22 -0300

    update

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './index'
 const veryConservativeStreep = async () =>{
   const response= await fetch('https://fintual.cl/api/real_assets/15077/days?from_date=%222022-01-01%22&to_date=%222022-12-21%22')
   var data = await response.json();
-  const assets_12 = data.data[315].attributes.total_net_assets ;
-  const assets_11 = data.data[314].attributes.total_net_assets ;
+  const assets_12 = data.data[315].attributes.net_asset_value ;
+  const assets_11 = data.data[314].attributes.net_asset_value ;
   let profit= (((assets_12-assets_11)/assets_11)*100).toFixed(3)+"%"
 
   document.getElementById('fund1').textContent=profit
@@ -16,8 +16,8 @@ veryConservativeStreep()
 const conservativeClooney = async () =>{
   let response_1= await fetch('https://fintual.cl/api/real_assets/188/days?from_date=%222022-01-01%22&to_date=%222022-12-21%22')
   var data_1 = await response_1.json();
-  let assets_12_1 = data_1.data[315].attributes.total_net_assets ;
-  let assets_11_1 = data_1.data[314].attributes.total_net_assets ;
+  let assets_12_1 = data_1.data[315].attributes.net_asset_value ;
+  let assets_11_1 = data_1.data[314].attributes.net_asset_value ;
   let profit_1= (((assets_12_1-assets_11_1)/assets_11_1)*100).toFixed(3)+"%"
 
   document.getElementById('fund2').textContent=profit_1
@@ -28,8 +28,8 @@ conservativeClooney()
 const moderatePit = async () =>{
   const response_2= await fetch('https://fintual.cl/api/real_assets/187/days?from_date=%222022-01-01%22&to_date=%222022-12-21%22')
   var data_2 = await response_2.json();
-  const assets_12 = data_2.data[315].attributes.total_net_assets ;
-  const assets_11 = data_2.data[314].attributes.total_net_assets ;
+  const assets_12 = data_2.data[315].attributes.net_asset_value ;
+  const assets_11 = data_2.data[314].attributes.net_asset_value ;
   let profit= (((assets_12-assets_11)/assets_11)*100).toFixed(3)+"%"
 
   document.getElementById('fund3').textContent=profit
@@ -40,8 +40,8 @@ moderatePit()
 const riskyNorris = async () =>{
   const response= await fetch('https://fintual.cl/api/real_assets/186/days?from_date=%222022-01-01%22&to_date=%222022-12-21%22')
   var data = await response.json();
-  const assets_12 = data.data[315].attributes.total_net_assets ;
-  const assets_11 = data.data[314].attributes.total_net_assets ;
+  const assets_12 = data.data[315].attributes.net_asset_value ;
+  const assets_11 = data.data[314].attributes.net_asset_value ;
   let profit= (((assets_12-assets_11)/assets_11)*100).toFixed(3)+"%"
 
   document.getElementById('fund4').textContent=profit
